b132a2407d9272725fb29d2d810052e1ee0ba249
Lanyue <colinn2417@outlook.com>
2019-06-18 13:41:56 +0800

    hello

---
+++ src/components/login.vue
@@ -0,0 +1,14 @@
+<template>
+    <form>
+        <input type="text" />
+        <input type="password" />
+        <input type="button" @click="submitClick" value="登录"/>
+    </form>
+</template>
+
+<script>
+  export default {
+    
+    }
+  
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import login from '@/components/login'
+import Hello from '@/components/HelloWorld'
+
 
 Vue.use(Router)
 
@@ -8,8 +10,14 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'login',
+      component: login,
+
+      path: 'hello',
+      name: 'hello',
+      component: Hello
     }
+
+    
   ]
 })
