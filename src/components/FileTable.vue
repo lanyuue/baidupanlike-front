3bf8f4b595851a92e6e10613f0d9f878f7cf9c57
Lanyue <colinn2417@outlook.com>
2019-06-25 01:40:29 +0800

    获取文件列表

--- src/components/FileTable.vue
+++ src/components/FileTable.vue
@@ -36,9 +36,9 @@
         md-auto-select
         style="max-width:10px"
       >
-        <md-table-cell style="text-align:left" md-label="文件名" md-sort-by="name">{{ item.name }}</md-table-cell>
-        <md-table-cell style="text-align:left" md-label="描述" md-sort-by="email">{{ item.email }}</md-table-cell>
-        <md-table-cell style="text-align:left" md-label="大小" md-sort-by="gender">{{ item.gender }}</md-table-cell>
+        <md-table-cell style="text-align:left" md-label="文件名" md-sort-by="name">{{ item.filename }}</md-table-cell>
+        <!-- <md-table-cell style="text-align:left" md-label="描述" md-sort-by="email">{{ item.totalSize / 1024 }}</md-table-cell> -->
+        <md-table-cell style="text-align:left" md-label="大小" md-sort-by="gender">{{ item.totalSize / 1024 }}</md-table-cell>
         <md-table-cell style="text-align:left" md-label="修改时间" md-sort-by="title">{{ item.title }}</md-table-cell>
       </md-table-row>
     </md-table>
@@ -68,146 +68,69 @@ export default {
     searched: [],
     selected: [],
     users: [
-      {
-        id: 1,
-        name: "Shawna Dubbin",
-        email: "sdubbin0@geocities.com",
-        gender: "Male",
-        title: "Assistant Media Planner"
-      },
-      {
-        id: 2,
-        name: "Odette Demageard",
-        email: "odemageard1@spotify.com",
-        gender: "Female",
-        title: "Account Coordinator"
-      },
-      {
-        id: 3,
-        name: "Vera Taleworth",
-        email: "vtaleworth2@google.ca",
-        gender: "Male",
-        title: "Community Outreach Specialist"
-      },
-      {
-        id: 4,
-        name: "Lonnie Izkovitz",
-        email: "lizkovitz3@youtu.be",
-        gender: "Female",
-        title: "Operator"
-      },
-      {
-        id: 5,
-        name: "Thatcher Stave",
-        email: "tstave4@reference.com",
-        gender: "Male",
-        title: "Software Test Engineer III"
-      },
-      {
-        id: 6,
-        name: "Karim Chipping",
-        email: "kchipping5@scribd.com",
-        gender: "Female",
-        title: "Safety Technician II"
-      },
-      {
-        id: 7,
-        name: "Helge Holyard",
-        email: "hholyard6@howstuffworks.com",
-        gender: "Female",
-        title: "Internal Auditor"
-      },
-      {
-        id: 8,
-        name: "Rod Titterton",
-        email: "rtitterton7@nydailynews.com",
-        gender: "Male",
-        title: "Technical Writer"
-      },
-      {
-        id: 9,
-        name: "Gawen Applewhite",
-        email: "gapplewhite8@reverbnation.com",
-        gender: "Female",
-        title: "GIS Technical Architect"
-      },
-      {
-        id: 10,
-        name: "Nero Mulgrew",
-        email: "nmulgrew9@plala.or.jp",
-        gender: "Female",
-        title: "Staff Scientist"
-      },
-      {
-        id: 11,
-        name: "Cybill Rimington",
-        email: "crimingtona@usnews.com",
-        gender: "Female",
-        title: "Assistant Professor"
-      },
-      {
-        id: 12,
-        name: "Maureene Eggleson",
-        email: "megglesonb@elpais.com",
-        gender: "Male",
-        title: "Recruiting Manager"
-      },
-      {
-        id: 13,
-        name: "Cortney Caulket",
-        email: "ccaulketc@cbsnews.com",
-        gender: "Male",
-        title: "Safety Technician IV"
-      },
-      {
-        id: 14,
-        name: "Selig Swynfen",
-        email: "sswynfend@cpanel.net",
-        gender: "Female",
-        title: "Environmental Specialist"
-      },
-      {
-        id: 15,
-        name: "Ingar Raggles",
-        email: "iragglese@cbc.ca",
-        gender: "Female",
-        title: "VP Sales"
-      },
-      {
-        id: 16,
-        name: "Karmen Mines",
-        email: "kminesf@topsy.com",
-        gender: "Male",
-        title: "Administrative Officer"
-      },
-      {
-        id: 17,
-        name: "Salome Judron",
-        email: "sjudrong@jigsy.com",
-        gender: "Male",
-        title: "Staff Scientist"
-      },
-      {
-        id: 18,
-        name: "Clarinda Marieton",
-        email: "cmarietonh@theatlantic.com",
-        gender: "Male",
-        title: "Paralegal"
-      },
-      {
-        id: 19,
-        name: "Paxon Lotterington",
-        email: "plotteringtoni@netvibes.com",
-        gender: "Female",
-        title: "Marketing Assistant"
-      },
-      {
-        id: 20,
-        name: "Maura Thoms",
-        email: "mthomsj@webeden.co.uk",
-        gender: "Male",
-        title: "Actuary"
-      }
+    //   {
+    //     id: 1,
+    //     name: "Shawna Dubbin",
+    //     id: "sdubbin0@geocities.com",
+    //     gender: "Male",
+    //     title: "Assistant Media Planner"
+    //   },
+    //   {
+    //     id: 2,
+    //     name: "Odette Demageard",
+    //     email: "odemageard1@spotify.com",
+    //     gender: "Female",
+    //     title: "Account Coordinator"
+    //   },
+    //   {
+    //     id: 3,
+    //     name: "Vera Taleworth",
+    //     email: "vtaleworth2@google.ca",
+    //     gender: "Male",
+    //     title: "Community Outreach Specialist"
+    //   },
+    //   {
+    //     id: 4,
+    //     name: "Lonnie Izkovitz",
+    //     email: "lizkovitz3@youtu.be",
+    //     gender: "Female",
+    //     title: "Operator"
+    //   },
+    //   {
+    //     id: 5,
+    //     name: "Thatcher Stave",
+    //     email: "tstave4@reference.com",
+    //     gender: "Male",
+    //     title: "Software Test Engineer III"
+    //   },
+    //   {
+    //     id: 6,
+    //     name: "Karim Chipping",
+    //     email: "kchipping5@scribd.com",
+    //     gender: "Female",
+    //     title: "Safety Technician II"
+    //   },
+    //   {
+    //     id: 7,
+    //     name: "Helge Holyard",
+    //     email: "hholyard6@howstuffworks.com",
+    //     gender: "Female",
+    //     title: "Internal Auditor"
+    //   },
+    //   {
+    //     id: 8,
+    //     name: "Rod Titterton",
+    //     email: "rtitterton7@nydailynews.com",
+    //     gender: "Male",
+    //     title: "Technical Writer"
+    //   },
+    //   {
+    //     id: 9,
+    //     name: "Gawen Applewhite",
+    //     email: "gapplewhite8@reverbnation.com",
+    //     gender: "Female",
+    //     title: "GIS Technical Architect"
+    //   }
     ]
   }),
   methods: {
@@ -231,8 +154,135 @@ export default {
       window.alert("Noop");
     }
   },
-  created() {
+  mounted() {
     this.searched = this.users;
+    this.axios
+      .get("/api/files/pull")
+      .then(successResponse => {
+        // console.log("jsonObj");
+        var response = successResponse.data.data;
+        // console.log(response);
+        // console.log(typeof response);
+        // console.log(JSON.parse(response))
+        // JSON.parse(response);
+        console.log(this.users);
+        this.users = response;
+        this.searched = this.users;
+        //   {
+        //     id: 10,
+        //     name: "Nero Mulgrew",
+        //     email: "nmulgrew9@plala.or.jp",
+        //     gender: "Female",
+        //     title: "Staff Scientist"
+        //   },
+        //   {
+        //     id: 11,
+        //     name: "Cybill Rimington",
+        //     email: "crimingtona@usnews.com",
+        //     gender: "Female",
+        //     title: "Assistant Professor"
+        //   },
+        //   {
+        //     id: 12,
+        //     name: "Maureene Eggleson",
+        //     email: "megglesonb@elpais.com",
+        //     gender: "Male",
+        //     title: "Recruiting Manager"
+        //   },
+        //   {
+        //     id: 13,
+        //     name: "Cortney Caulket",
+        //     email: "ccaulketc@cbsnews.com",
+        //     gender: "Male",
+        //     title: "Safety Technician IV"
+        //   },
+        //   {
+        //     id: 14,
+        //     name: "Selig Swynfen",
+        //     email: "sswynfend@cpanel.net",
+        //     gender: "Female",
+        //     title: "Environmental Specialist"
+        //   },
+        //   {
+        //     id: 15,
+        //     name: "Ingar Raggles",
+        //     email: "iragglese@cbc.ca",
+        //     gender: "Female",
+        //     title: "VP Sales"
+        //   },
+        //   {
+        //     id: 16,
+        //     name: "Karmen Mines",
+        //     email: "kminesf@topsy.com",
+        //     gender: "Male",
+        //     title: "Administrative Officer"
+        //   },
+        //   {
+        //     id: 17,
+        //     name: "Salome Judron",
+        //     email: "sjudrong@jigsy.com",
+        //     gender: "Male",
+        //     title: "Staff Scientist"
+        //   },
+        //   {
+        //     id: 18,
+        //     name: "Clarinda Marieton",
+        //     email: "cmarietonh@theatlantic.com",
+        //     gender: "Male",
+        //     title: "Paralegal"
+        //   },
+        //   {
+        //     id: 19,
+        //     name: "Paxon Lotterington",
+        //     email: "plotteringtoni@netvibes.com",
+        //     gender: "Female",
+        //     title: "Marketing Assistant"
+        //   },
+        //   {
+        //     id: 20,
+        //     name: "Maura Thoms",
+        //     email: "mthomsj@webeden.co.uk",
+        //     gender: "Male",
+        //     title: "Actuary"
+        //   }
+        // ];
+        
+        console.log(this.users);
+        // var arr = [];
+        // for (let i in response) {
+        //   let o = {};
+        //   o[i] = response[i];
+        //   arr.push(o);
+        // }
+        // console.log(typeof(arr));
+
+        // var jsonObj = JSON.parse(successResponse);
+        // console.log(jsonObj);
+        // var jsonStr1 = JSON.stringify(jsonObj);
+        // console.log(jsonStr1 + "jsonStr1");
+        // var jsonArr = [];
+        // for (var i = 0; i < jsonObj.length; i++) {
+        //   jsonArr[i] = jsonObj[i];
+        // }
+        // console.log(typeof jsonArr);
+        if (successResponse.data.code == 200) {
+          // var jsonArr = [];
+          // for (var i = 0; i < jsonObj.length; i++) {
+          //   jsonArr[i] = successResponse.data.data[i];
+          // }
+          // // location.reload();
+          // console.log(typeof jsonArr);
+          // console.log("typeof(jsonArr)");
+          // this.users = jsonArr;
+          // // console.log(this.users[0])
+        } else {
+          // this.alert = successResponse.data.message;
+        }
+      })
+      .then(() => {
+        this.closeA();
+      })
+      .catch(failResponse => {});
   }
 };
 </script>
